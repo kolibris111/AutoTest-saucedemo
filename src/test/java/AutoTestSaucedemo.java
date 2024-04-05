0be785962bdab81ef422ad1dfb6797e6a6778539
kolibris111 <palubinskaitrasele1@gmail.com>
2024-04-05 09:23:23 +0300

    Saucedemo testai

--- src/test/java/AutoTestSaucedemo.java
+++ src/test/java/AutoTestSaucedemo.java
@@ -123,7 +123,7 @@ public class AutoTestSaucedemo {
     }
     @Test
     public void Test2(){
-        //
+        //Prekių krepšelio patvirtinimas ir pirkimas
         _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");//Irasomas username
         try {
             TimeUnit.SECONDS.sleep(2);
@@ -215,5 +215,243 @@ public class AutoTestSaucedemo {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+    @Test
+    public void Test3(){
+        //Pirkimas nurodant vardą  "Name:" ir "Surname:" su skaičiais
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");//Irasomas username
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");//Irasomas password
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("login-button")).click();//Spaudziame prisijungti
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        _globalDriver.get("https://www.saucedemo.com/inventory.html");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<WebElement> productItems = _globalDriver.findElements(By.className("inventory_item"));
+        //Pasirinkti random preke apzvalgai
+        Random random = new Random();
+        int randomIndex = random.nextInt(productItems.size());
+        WebElement randomProduct = productItems.get(randomIndex);
+
+        randomProduct.findElement(By.className("inventory_item_img")).click();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("add-to-cart")).click();//Spaudziame ideti i krepseli
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]/a")).click();//Pasirenkama prekiu krepselio perziura
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("checkout")).click();//Pasirenkamas apmokejimas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("first-name")).sendKeys("Dobilas777");//Irasomas vardas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("last-name")).sendKeys("Keturlapis111");//Irasoma pavarde
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("postal-code")).sendKeys("28910");//Irasomas pasto kodas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("continue")).click();//Paspaudziama Continue
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("finish")).click();//Paspaudziama Finish
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("back-to-products")).click();//Spaudziama grizti i pagrindini puslapi
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void Test4(){
+        //Pirkimas nurodant vardą  "Name:" ir "Surname:" tik skaičiais
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");//Irasomas username
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");//Irasomas password
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("login-button")).click();//Spaudziame prisijungti
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        _globalDriver.get("https://www.saucedemo.com/inventory.html");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<WebElement> productItems = _globalDriver.findElements(By.className("inventory_item"));
+        //Pasirinkti random preke apzvalgai
+        Random random = new Random();
+        int randomIndex = random.nextInt(productItems.size());
+        WebElement randomProduct = productItems.get(randomIndex);
+
+        randomProduct.findElement(By.className("inventory_item_img")).click();
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("add-to-cart")).click();//Spaudziame ideti i krepseli
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[1]/div[3]/a")).click();//Pasirenkama prekiu krepselio perziura
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("checkout")).click();//Pasirenkamas apmokejimas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("first-name")).sendKeys("777");//Irasomas vardas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("last-name")).sendKeys("111");//Irasoma pavarde
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("postal-code")).sendKeys("28910");//Irasomas pasto kodas
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("continue")).click();//Paspaudziama Continue
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("finish")).click();//Paspaudziama Finish
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("back-to-products")).click();//Spaudziama grizti i pagrindini puslapi
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void Test5() {
+        //Tikrinama ar veikia soc.tinklų ikonėlės ir  privatumo politikos mygtukas
+        _globalDriver.findElement(By.id("user-name")).sendKeys("standard_user");//Irasomas username
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("password")).sendKeys("secret_sauce");//Irasomas password
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.id("login-button")).click();//Spaudziame prisijungti
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/footer/ul/li[1]/a")).click();//Tikrinama ar veikia "Twitter" ikonėlė
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/footer/ul/li[2]/a")).click();//Tikrinama ar veikia "Facebook" ikonėlė
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/footer/ul/li[3]/a")).click();//Tikrinama ar veikia "LinkedIn" ikonėlė
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _globalDriver.findElement(By.xpath("/html/body/div/div/footer/div")).click();//Tikrinama ar galima perskaityti Privacy Policy
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Testas turetu buti FAIL, bet yra PASS, nors galimybės perskaityti Privacy Policy nėra.
     }
 }
